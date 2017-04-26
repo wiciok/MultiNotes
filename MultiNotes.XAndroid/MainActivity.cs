1a571a59d28d63805f38ce8aff9ec4351a5f6c59
malczuuu <damian.mal7@gmail.com>
2017-04-26 19:00:49 +0200

    Xamarin - początki

---
+++ MultiNotes.XAndroid/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+namespace MultiNotes.XAndroid
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.TopMenus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
+            SetActionBar(toolbar);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
+                ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
