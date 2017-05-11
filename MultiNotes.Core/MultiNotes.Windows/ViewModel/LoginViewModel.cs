26b62d64707872a61db18a219ccec3d502304ddd
pakopako <pawciopako@gmail.com>
2017-05-11 16:50:54 +0200

    Próba wykonania rejestracji i logowania (jeszcze nie działa).

---
+++ MultiNotes.Core/MultiNotes.Windows/ViewModel/LoginViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiNotes.Core;
+using System.Web.Http;
+using System.Windows;
+
+namespace MultiNotes.Windows.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task Login()
+        {
+            ConnectionApi.configure();
+            UserMethod methods = new UserMethod(ConnectionApi.httpClient);
+
+            try
+            {
+                await methods.login("nowiutki", "nowiutki");
+            }
+            catch(HttpResponseException e)
+            {
+                if(e.Response.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    MessageBox.Show("Error: NotFound");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ MultiNotes.Core/MultiNotes.Windows/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MultiNotes.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Windows;
+
+namespace MultiNotes.Windows.ViewModel
+{
+    public class RegisterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task Register()
+        {
+            ConnectionApi.configure();
+            UserMethod methods = new UserMethod(ConnectionApi.httpClient);
+
+            try
+            {
+                await methods.register("nowiutki", "nowiutki");
+            }
+            catch(Exception ex)
+            {
+                    MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
