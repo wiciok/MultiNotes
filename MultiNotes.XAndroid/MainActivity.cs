7ea7f5ef7b443ce6989857126605606372869c79
malczuuu <damian.mal7@gmail.com>
2017-05-12 09:35:49 +0200

    Dodano Toolbar z menu, SignIn i SignUp

--- MultiNotes.XAndroid/MainActivity.cs
+++ MultiNotes.XAndroid/MainActivity.cs
@@ -2,10 +2,11 @@
 using Android.Widget;
 using Android.OS;
 using Android.Views;
+using Android.Content.PM;
 
 namespace MultiNotes.XAndroid
 {
-    [Activity(MainLauncher = true)]
+    [Activity(MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Activity
     {
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -21,7 +22,7 @@ namespace MultiNotes.XAndroid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.Toolbar);
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.ToolbarMain);
             SetActionBar(toolbar);
         }
 
@@ -29,8 +30,11 @@ namespace MultiNotes.XAndroid
         {
             Toast.MakeText(this, "Action selected: " + item.TitleFormatted,
                 ToastLength.Short).Show();
+            if (item.ItemId == Resource.Id.MenuAccount)
+            {
+                StartActivity(typeof(SignInActivity));
+            }
             return base.OnOptionsItemSelected(item);
         }
     }
 }
- 
---
+++ MultiNotes.XAndroid/SignInActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Content.PM;
+
+namespace MultiNotes.XAndroid
+{
+    [Activity(MainLauncher = false, ParentActivity = typeof(MainActivity), ScreenOrientation = ScreenOrientation.Portrait)]
+    public class SignInActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.SignIn);
+            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.ToolbarSignIn);
+            SetActionBar(toolbar);
+
+            Button moveToSignUpButton = (Button)FindViewById(Resource.Id.MoveToSignUpButton);
+            moveToSignUpButton.Click += delegate
+            {
+                StartActivity(typeof(SignUpActivity));
+            };
+        }
+    }
+}
