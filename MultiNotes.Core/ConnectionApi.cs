8451978f577b22f2137bdb09dcc53f0c330de943
q w <wiciok1@gmail.com>
2017-05-15 11:52:08 +0200

    propper configuration for deployed webapi

--- MultiNotes.Core/ConnectionApi.cs
+++ MultiNotes.Core/ConnectionApi.cs
@@ -12,10 +12,10 @@ namespace MultiNotes.Core
     class ConnectionApi//singleton???
     {
         public static HttpClient httpClient = new HttpClient();
-        private static int port = 63252;
+        private static int port = 80;
         public static void configure()
         {
-            httpClient.BaseAddress = new Uri("http://localhost:" + port);
+            httpClient.BaseAddress = new Uri("http://217.61.4.233:8080/MultiNotes.Server/");
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
