d82891f643e3033addfd9217dd54d06c489e6317
q w <wiciok1@gmail.com>
2017-05-26 17:22:36 +0200

    connecton string now available to choose by preprocesor macro

--- MultiNotes.Core/ConnectionApi.cs
+++ MultiNotes.Core/ConnectionApi.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿#define DEBUG_CONFIGURATION
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+
 namespace MultiNotes.Core
 {
     public class ConnectionApi//singleton???
     {
-        public static HttpClient httpClient = new HttpClient();
+#if DEBUG_CONFIGURATION
         private static int port = 63252;
+        private static string DBAddress = "http://localhost:";
+#else
+        private static int port = 80;
+        private static string DBAddress = "http://217.61.4.233:8080/MultiNotes.Server/";
+#endif
+
+        public static HttpClient httpClient = new HttpClient();  
         private static bool configured = false;
         public static void configure()
         {
             if (!configured)
             {
-                httpClient.BaseAddress = new Uri("http://localhost:" + port);
+                httpClient.BaseAddress = new Uri(DBAddress + port);
                 httpClient.DefaultRequestHeaders.Accept.Clear();
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 configured = true;
