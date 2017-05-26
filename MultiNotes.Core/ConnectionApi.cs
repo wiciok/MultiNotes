bebb8f26e7ae89286cec334194aaea0945fb9f87
Wojciech Książek <wojtekksiazekk@gmail.com>
2017-05-26 16:10:43 +0200

    Poprawka w connectionapi.

--- MultiNotes.Core/ConnectionApi.cs
+++ MultiNotes.Core/ConnectionApi.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-
 namespace MultiNotes.Core
 {
-    class ConnectionApi//singleton???
+    public class ConnectionApi//singleton???
     {
         public static HttpClient httpClient = new HttpClient();
-        private static int port = 80;
+        private static int port = 63252;
+        private static bool configured = false;
         public static void configure()
         {
-            httpClient.BaseAddress = new Uri("http://217.61.4.233:8080/MultiNotes.Server/");
-            httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            if (!configured)
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:" + port);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                configured = true;
+            }
         }
     }
 }
