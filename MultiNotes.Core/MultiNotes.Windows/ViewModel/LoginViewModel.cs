0be09cf8b29fb3d3cfafcef2cefc02844c22f61f
q w <wiciok1@gmail.com>
2017-05-26 17:08:26 +0200

    # Conflicts: #	MultiNotes.Core.sln #	MultiNotes.Core/Main.cs #	MultiNotes.Core/UserMethod.cs #	MultiNotes.SimpleClient/MultiNotes.SimpleClient.csproj #	MultiNotes.SimpleClient/packages.config

---
+++ MultiNotes.Core/MultiNotes.Windows/ViewModel/LoginViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiNotes.Core;
+using System.Web.Http;
+using System.Windows;
+using MultiNotes.Windows.View;
+
+namespace MultiNotes.Windows.ViewModel
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MultiNotesLoginWindow loginWindow;
+
+        public LoginViewModel(MultiNotesLoginWindow loginWindow)
+        {
+            this.loginWindow = loginWindow;
+        }
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void MakeLoginTask(string email, string password)
+        {
+            Login(email, password);
+        }
+
+        public async void Login(string email, string password)
+        {
+            
+            UserMethod methods = new UserMethod(ConnectionApi.httpClient);
+
+            try
+            {
+                    await methods.login(email, password);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Wrong username or password");
+                return;
+            }
+
+            Application.Current.Dispatcher.Invoke(delegate
+            {
+                MultiNotesMainWindow mainWindow = new MultiNotesMainWindow();
+                loginWindow.Close();
+                mainWindow.Show();
+            });
+        }
+    }
+}
---
+++ MultiNotes.Core/MultiNotes.Windows/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,44 @@
+﻿using MultiNotes.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Windows;
+
+namespace MultiNotes.Windows.ViewModel
+{
+    public class RegisterViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void MakeRegisterTask(string email, string password)
+        {
+            Task testTask = new Task(() => Register(email, password).Wait());
+            testTask.Start();
+        }
+
+        public async Task Register(string email, string password)
+        {
+            ConnectionApi.configure();
+            UserMethod methods = new UserMethod(ConnectionApi.httpClient);
+
+            try
+            {
+                await methods.register(email, password);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
