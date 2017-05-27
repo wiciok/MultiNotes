4fba22ec109ceb55e9f529e2fe2f1d1cd48a1d65
DancingWithNoobs <DancingWithNoobs@gmail.com>
2017-05-27 11:31:15 +0200

    Added internet connection checker and base for Note apis

---
+++ MultiNotes.Core/Util/InternetConection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiNotes.Core.Util
+{
+    class InternetConection
+    {
+        public static bool CheckInternetConnection()
+        {
+            return false;
+        }
+    }
+}
---
+++ MultiNotes.Core/WindowsNoteApi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiNotes.Model;
+
+namespace MultiNotes.Core
+{
+    class WindowsNoteApi : INoteApi
+    {
+        private string userId;
+        public WindowsNoteApi(string userId)
+        {
+            this.userId = userId;
+        }
+        public void AddNote(Note note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Note> GetNoteById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Note>> GetAllNotes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteNoteById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateNote(string id, Note newNote)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
