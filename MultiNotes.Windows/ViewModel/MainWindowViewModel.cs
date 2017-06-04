77de26ff7d3b084b5c38bb72a3b0241b9d2b8310
pakopako <pawciopako@gmail.com>
2017-06-04 18:12:56 +0200

    Prace nad notatką ciąg dalszy.

--- MultiNotes.Windows/ViewModel/MainWindowViewModel.cs
+++ MultiNotes.Windows/ViewModel/MainWindowViewModel.cs
@@ -16,26 +16,50 @@ namespace MultiNotes.Windows.ViewModel
         public MainWindowViewModel(Action closeAction)
         {
             _closeAction = closeAction;
-        
+            methods = new UserMethod(ConnectionApi.HttpClient);
+
+            methods.PreparedAuthenticationRecord();
+            _authenticationRecord = methods.Record;
+            MessageBox.Show(_authenticationRecord.Email + " " + _authenticationRecord.PasswordHash);
+            GetAllNotes();
+
         }
         public event PropertyChangedEventHandler PropertyChanged;
         private readonly Action _closeAction;
         private readonly AuthenticationRecord _authenticationRecord;
+        private string token;
         private ObservableCollection<Note> notes;
+        private User user;
+        UserMethod methods;
 
         public async void GetAllNotes()
-        { 
-            var noteApi = new NoteApi(_authenticationRecord, "id");
+        {
+            getToken();
+            MessageBox.Show(token);
+            user = await methods.GetUserInfo(token, _authenticationRecord.Email);
+            var noteApi = new NoteApi(_authenticationRecord, user.Id);
             try
             {
                 IEnumerable<Note> tempNotes = await noteApi.GetAllNotesAsync();
                 notes = new ObservableCollection<Note>(tempNotes);
+                for(int i = 0; i < notes.Count; i++)
+                {
+                    MessageBox.Show(notes[i].Content + "\n");
+                }
             }
             catch (Exception e)
             {
                 MessageBox.Show("An error has occured. {0}", e.Message);
                 return;
             }
+        }
+
+
+
+        private async void getToken()
+        {
+            AuthenticationToken authToken = new AuthenticationToken(ConnectionApi.HttpClient);
+            token = await authToken.PostAuthRecordAsync(_authenticationRecord);
         }
 
         protected virtual void OnPropertyChanged(string propName)
