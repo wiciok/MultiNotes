93a608ca54f74cd77cada933a073a710733962c3
DancingWithNoobs <DancingWithNoobs@gmail.com>
2017-06-05 21:22:54 +0200

    Added application icon and removed temporary note

--- MultiNotes.Windows/App.xaml.cs
+++ MultiNotes.Windows/App.xaml.cs
@@ -14,12 +14,6 @@ namespace MultiNotes.Windows
         {
             MultiNotesLoginWindow loginWindow = new MultiNotesLoginWindow();
             loginWindow.Show();
-
-            //todo: usunąć to potem, teraz jest tylko w celach testowych
-            string loremIpsum =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies diam id tellus ullamcorper, ac pulvinar mi venenatis. Quisque sed gravida purus. Etiam id tortor eros. Vivamus vel pretium mauris, id tincidunt lectus. Nullam a bibendum dolor. Phasellus iaculis arcu massa, nec egestas nibh efficitur eu. Pellentesque ornare ullamcorper sem sed sagittis. ";
-            SingleNoteWindow singleNoteWindow=new SingleNoteWindow(new Note {Content = loremIpsum, CreateTimestamp = DateTime.Now});
-            singleNoteWindow.Show();
         }
     }
 }
