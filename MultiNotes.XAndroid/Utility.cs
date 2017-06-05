b095b628cd2a7280d8a5bdd8da921978e8c4d9a1
malczuuu <damian.mal7@gmail.com>
2017-06-05 23:31:58 +0200

    Small beginning of implementing testing network connection

---
+++ MultiNotes.XAndroid/Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Net;
+
+namespace MultiNotes.XAndroid
+{
+    public sealed class Utility
+    {
+        public static bool IsNetworkAvailable(Context context)
+        {
+            ConnectivityManager connectivityManager
+                  = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
+            NetworkInfo activeNetworkInfo = connectivityManager.ActiveNetworkInfo;
+            return activeNetworkInfo != null && activeNetworkInfo.IsConnected;
+        }
+
+
+        private Utility()
+        {
+        }
+    }
+}
