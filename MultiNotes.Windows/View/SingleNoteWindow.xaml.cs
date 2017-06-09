a4b93ccddcd9eb9b0555028639dde36c3ce56b2b
q w <wiciok1@gmail.com>
2017-06-09 14:48:53 +0200

    added new, custom resize grip

--- MultiNotes.Windows/View/SingleNoteWindow.xaml.cs
+++ MultiNotes.Windows/View/SingleNoteWindow.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
 using System.Windows.Shapes;
 using MultiNotes.Model;
 using MultiNotes.Windows.Util;
@@ -46,11 +50,6 @@ namespace MultiNotes.Windows.View
             }
         }
 
-        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            DragMove();
-        }
-
         private void closeBtn_Click(object sender, RoutedEventArgs e)
         {
             Close();
@@ -65,5 +64,29 @@ namespace MultiNotes.Windows.View
                     MainGrid.Background = rectangle.Fill;
             }
         }
+
+        //-------------custom grip code--------------
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        //Attach this to the MouseDown event of your drag control to move the window in place of the title bar
+        private void WindowDrag(object sender, MouseButtonEventArgs e) // MouseDown
+        {
+            ReleaseCapture();
+            SendMessage(new WindowInteropHelper(this).Handle,
+                0xA1, (IntPtr)0x2, (IntPtr)0);
+        }
+
+        //Attach this to the PreviewMousLeftButtonDown event of the grip control in the lower right corner of the form to resize the window
+        private void WindowResize(object sender, MouseButtonEventArgs e) //PreviewMousLeftButtonDown
+        {
+            HwndSource hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+            SendMessage(hwndSource.Handle, 0x112, (IntPtr)61448, IntPtr.Zero);
+        }
+        //--------------------------------------------
     }
 }
