fd637d4b3ef2429588a36d4597a875ba069f962e
DancingWithNoobs <DancingWithNoobs@gmail.com>
2017-06-11 09:41:23 +0200

    Exception handling in InternetConnection Check

--- MultiNotes.Core/Util/InternetConnection.cs
+++ MultiNotes.Core/Util/InternetConnection.cs
@@ -14,11 +14,18 @@ namespace MultiNotes.Core.Util
     {
         public static async Task<bool> IsInternetConnectionAvailable()
         {
-            var response = await ConnectionApi.HttpClient.GetAsync("api/id/");
-
-            if (response.StatusCode == HttpStatusCode.Created)
+            try
             {
-                return true;
+                var response = await ConnectionApi.HttpClient.GetAsync("api/id/");
+
+                if (response.StatusCode == HttpStatusCode.Created)
+                {
+                    return true;
+                }
+            }
+            catch (Exception)
+            {
+                return false;
             }
             return false;
         }
